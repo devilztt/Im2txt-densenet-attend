b14e5f6cb38a8d3b1530965792b6ab180dd94095
ztt-ubuntu <zhoutaotaox@foxmail.com>
2018-09-26 11:21:10 +0800

    remove keras

--- im2txt/ops/inputs.py
+++ im2txt/ops/inputs.py
@@ -22,7 +22,7 @@ from __future__ import print_function
 
 import tensorflow as tf
 import numpy as np
-from keras.preprocessing import sequence
+
 
 
 def parse_sequence_example(serialized, image_feature, caption_feature):
